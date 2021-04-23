f453be70fc4cc636f09b71cb6619b140b9274a05
ev1illyn <evillyndsoliveiras@gmail.com>
2021-04-22 22:18:14 -0300

    config: consumindo api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
-
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>index.tsx function Home()</h1>
+    <h1>index.tsx function Home()
+      {JSON.stringify(props.episodes)}</h1>
   );
+
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
 }
